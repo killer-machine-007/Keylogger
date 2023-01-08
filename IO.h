bb61200b0ae57459189feb8917798c552ba891b0
moksh <altiarahad@gmail.com>
2023-01-08 20:56:17 +0530

    started IO.h

---
+++ IO.h
@@ -0,0 +1,15 @@
+#ifndef IO_H
+#define IO_H
+
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include "windows.h"
+#include "Helper.h"
+#include "Base64.h"
+
+namespace IO {
+    
+}
+
+#endif
